85da6d7eeb81d0885051455ef44afbe24add9f0b
oMalyugina <o.v.malyugina@gmail.com>
2019-08-14 11:18:47 +0200

    make train skript. create models zoo

---
+++ models.py
@@ -0,0 +1,82 @@
+from keras.models import Sequential, Model
+from keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.optimizers import Adam
+from keras.applications.mobilenet_v2 import MobileNetV2
+
+
+def create_first_model(input_shape=[64, 64, 3]):
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Flatten())
+    model.add(Dense(64))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(9))
+    model.add(Activation('softmax'))
+
+    model.compile(optimizer=Adam(),
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['sparse_categorical_accuracy'])
+    return model
+
+
+def create_second_model(input_shape=[64, 64, 3]):
+    model = Sequential()
+    model.add(Conv2D(64, (3, 3), input_shape=input_shape))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Flatten())
+    model.add(Dense(100))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(100))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(9))
+    model.add(Activation('softmax'))
+
+    model.compile(optimizer=Adam(),
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['sparse_categorical_accuracy'])
+    return model
+
+
+def create_pretrained_mobileNet(input_shape=[64, 64, 3], is_base_layer_trainable: bool = True):
+    base_model = MobileNetV2(input_shape=input_shape, weights='imagenet', include_top=False)
+
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(1024, activation='relu')(x)
+    x = Dense(1024, activation='relu')(x)
+    x = Dense(512, activation='relu')(x)
+    predictions = Dense(9, activation='softmax')(x)
+    model = Model(inputs=base_model.input, outputs=predictions)
+
+    for layer in base_model.layers:
+        layer.trainable = is_base_layer_trainable
+
+    model.compile(optimizer=Adam(),
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['sparse_categorical_accuracy'])
+    return model
